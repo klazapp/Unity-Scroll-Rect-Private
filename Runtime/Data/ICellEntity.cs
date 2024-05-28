5d15c5e9e9e9dcf5e87b31dbb359f4c2f8c8307c
klazapp <klazapp@klazapp.com>
2024-05-28 14:01:42 +0800

    Update ICellEntity.cs

--- Runtime/Data/ICellEntity.cs
+++ Runtime/Data/ICellEntity.cs
@@ -3,7 +3,7 @@ namespace com.Klazapp.Utility
     public interface ICellEntity<T> where T : CellComponent
     {
         public T CellComponent { get; set; }
-        public void SetData(T comp);
+        public void SetData(T comp, ScrollEventManager scrollEventManager = null);
         public void OnButtonPressed();
     }
 }

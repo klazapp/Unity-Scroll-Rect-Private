dcc18e008ac40e0a7df88fbe3b0b26225c5a6051
klazapp <klazapp@klazapp.com>
2024-05-28 13:40:52 +0800

    Update ScrollEventManager.cs

--- Runtime/Data/ScrollEventManager.cs
+++ Runtime/Data/ScrollEventManager.cs
@@ -5,8 +5,8 @@ namespace com.Klazapp.Utility
 {
     public class ScrollEventManager : MonoBehaviour
     {
-        public event Action<CellEntity> OnTriggerCellClicked;
-        public void InvokeCellClicked(CellEntity cellEntity)
+        public static event Action<CellEntity> OnTriggerCellClicked;
+        public static void InvokeCellClicked(CellEntity cellEntity)
         {
             OnTriggerCellClicked?.Invoke(cellEntity);
         }
